f2e62588851b8ce9bdd62f7363c2c9203ed2a7c3
ahmedkhaled130 <ak782060@gmail.com>
2024-07-14 16:33:05 +0300

    creat product state

---
+++ lib/cubit/product_state.dart
@@ -0,0 +1,9 @@
+abstract class ProductState {}
+
+class InitProductState extends ProductState {}
+
+class GetProductLoadingState extends ProductState {}
+
+class GetProductSuccessState extends ProductState {}
+
+class GetProductErrorState extends ProductState {}
