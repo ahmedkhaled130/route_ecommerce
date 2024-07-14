7bd84e352f3d1c0b86ba03a4f2d797d526147907
ahmedkhaled130 <ak782060@gmail.com>
2024-07-14 16:31:12 +0300

    creating dart file to save some consts

---
+++ lib/core/strings.dart
@@ -0,0 +1,7 @@
+   import 'package:flutter/material.dart';
+
+const String baseUrl = "https://dummyjson.com";
+   BorderSide borderSide= BorderSide(width: 2, color: Colors.blueAccent.shade100);
+    const String getAllProducts='/product';
+
+
